a363eaf7ed856b7effac77596b6f4309b363fe61
Darya Drutsko <d.drutsko@andersenlab.com>
2025-02-02 01:41:56 +0100

    feat

--- src/main/java/com/deye/web/async/RabbitMqProducer.java
+++ src/main/java/com/deye/web/async/RabbitMqProducer.java
@@ -1,6 +1,7 @@
 package com.deye.web.async;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,10 @@ public class RabbitMqProducer {
     private final RabbitTemplate rabbitTemplate;
 
     public void send(String exchange, String routingKey, Object data) {
-        rabbitTemplate.convertAndSend(exchange, routingKey, data);
+        rabbitTemplate.convertAndSend(exchange, routingKey, data, message -> {
+            MessageProperties properties = message.getMessageProperties();
+            properties.setContentType("application/json");
+            return message;
+        });
     }
 }
