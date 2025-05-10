240967ad6ab33f24987d57a9c541270325eb6fe0
furssov <fursovd70@gmail.com>
2025-05-10 23:18:24 +0300

    prometheus

--- src/main/java/com/deye/web/security/config/WebSecurityConfiguration.java
+++ src/main/java/com/deye/web/security/config/WebSecurityConfiguration.java
@@ -26,6 +26,7 @@ public class WebSecurityConfiguration {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/categories/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/actuator/prometheus").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         // Swagger
                         .requestMatchers("/swagger-ui/**").permitAll()
